169ba28c664ea80ced3e1aabaea20ff8d84571cf
yuliia levytska <lupin773311asd@gmail.com>
2025-04-26 00:29:05 +0100

    add

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,14 @@
 import Contact from '../Contact/Contact';
+import styles from './ContactList.module.css';
 
 export default function ContactList({ contacts, onDelete }) {
   return (
-    <ul>
+    <ul className={styles.list}>
       {contacts.map(contact => (
-        <li key={contact.id}>
+        <li key={contact.id} className={styles.item}>
           <Contact contact={contact} onDelete={onDelete} />
         </li>
       ))}
     </ul>
   );
 }
-
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,23 +1,28 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter, selectNameFilter } from '../../redux/filtersSlice';
-import css from './SearchBox.module.css';
+import styles from './SearchBox.module.css';
 
 export default function SearchBox() {
   const dispatch = useDispatch();
   const filter = useSelector(selectNameFilter);
 
   return (
-    <label>
-      Search
+    <div className={styles.wrapper}>
       <input
         type="text"
+        className={styles.input}
         placeholder="Search contacts"
         value={filter}
         onChange={e => dispatch(setFilter(e.target.value))}
       />
-    </label>
+      <button type="button" className={styles.button}>
+        Search
+      </button>
+    </div>
   );
 }
+
+
 
 
 
