a8d046e5497d3803c288802d089d01670fe32d8e
yuliia levytska <lupin773311asd@gmail.com>
2025-04-25 12:02:46 +0100

    ghj

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter, selectNameFilter } from '../../redux/filtersSlice';
+import css from './SearchBox.module.css';
 
 export default function SearchBox() {
   const dispatch = useDispatch();
